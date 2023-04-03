2101433460c7e8b02d5c7bffa015c7a58aaac546
David Francis <2251968F@student.gla.ac.uk>
2023-04-03 18:21:34 +0100

    Adding customisable source currency

--- app.py
+++ app.py
@@ -8,12 +8,19 @@ c = CurrencyRates()
 
 @app.route('/', methods=['GET', 'POST'])
 def currency_converter():
+    """Convert currency and display the conversion result."""
     if request.method == 'POST':
         amount = float(request.form['amount'])
-        target_currency = request.form['currency']
-        converted_amount = c.convert('USD', target_currency, amount)
-        return render_template('conversion_result.html', converted_amount=converted_amount)
+        source_currency = request.form['source_currency'].upper()
+        target_currency = request.form['target_currency'].upper()
+        try:
+            converted_amount = c.convert(source_currency, target_currency, amount)
+            return render_template('conversion_result.html', converted_amount=converted_amount,
+                                   source_currency=source_currency, target_currency=target_currency)
+        except Exception as e:
+            return render_template('currency_converter.html', error=str(e))
     return render_template('currency_converter.html')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
