2ac6e02feddfc167cc9f4c04c5aa801667eb03fa
davidfr1999 <64556566+davidfr1999@users.noreply.github.com>
2023-04-03 17:08:00 +0100

    Update app.py
    adding currency conversion functionality

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 import os
+from forex_python.converter import CurrencyRates
+from flask import request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
+c = CurrencyRates()
+
+@app.route('/currency_converter')
+def currency_converter():
+    return render_template('currency_converter.html')
+
+@app.route('/convert_currency', methods=['POST'])
+def convert_currency():
+    amount = float(request.form['amount'])
+    target_currency = request.form['currency']
+    converted_amount = c.convert('USD', target_currency, amount)
+    return render_template('conversion_result.html', converted_amount=converted_amount)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
