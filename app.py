1d2f6f00cb13b5a223caa7d8042f34d6be0a18ec
davidfr1999 <64556566+davidfr1999@users.noreply.github.com>
2023-04-03 17:48:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 import os
 from forex_python.converter import CurrencyRates
-from flask import request, render_template
 
 app = Flask(__name__)
 
 c = CurrencyRates()
 
-@app.route('/currency_converter')
+@app.route('/', methods=['GET', 'POST'])
 def currency_converter():
+    if request.method == 'POST':
+        amount = float(request.form['amount'])
+        target_currency = request.form['currency']
+        converted_amount = c.convert('USD', target_currency, amount)
+        return render_template('conversion_result.html', converted_amount=converted_amount)
     return render_template('currency_converter.html')
-
-@app.route('/convert_currency', methods=['POST'])
-def convert_currency():
-    amount = float(request.form['amount'])
-    target_currency = request.form['currency']
-    converted_amount = c.convert('USD', target_currency, amount)
-    return render_template('conversion_result.html', converted_amount=converted_amount)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
